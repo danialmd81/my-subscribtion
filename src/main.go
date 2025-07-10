c163da57ae5bdc64fc28bd06b012062496decb20
Danial Mobini <danialmobinidh81@gmail.com>
2025-07-10 21:53:45 +0330

    Refactor Travis CI configuration and enable module execution in main function

--- src/main.go
+++ src/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/danialmd81/my-subscribtion/all"
+	"github.com/danialmd81/my-subscribtion/subs"
+	"github.com/danialmd81/my-subscribtion/telegram"
 )
 
 func main() {
-	// telegram.Run()
-	// subs.Run()
+	telegram.Run()
+	subs.Run()
 	all.Run()
 }
